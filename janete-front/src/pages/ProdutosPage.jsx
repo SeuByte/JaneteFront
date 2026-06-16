8ec4b74c2b078c69925cfe533e31e50150b877e2
Gabriel Marques <gabrielmarquesterr@gmail.com>
2026-06-16 17:15:12 -0300

    Manutenção da NavbBar.

--- janete-front/src/pages/ProdutosPage.jsx
+++ janete-front/src/pages/ProdutosPage.jsx
@@ -89,6 +89,69 @@ export default function Produtos() {
     </div>
     <button class="hover:bg-green-700 text-gray-50 bg-green-500 py-2 rounded-md cursor-pointer">Adicionar ao carrinho</button>
   </div>
+</div>
+
+<div class="w-60 h-80 bg-gray-50 p-3 flex flex-col gap-1 rounded-2xl">
+
+  
+  <div class="h-48">
+    <img src="./carIcon.png" alt="" />
+  </div>
+
+
+
+  <div class="flex flex-col gap-4">
+    <div class="flex flex-row justify-between">
+      <div class="flex flex-col">
+        <span class="text-xl font-bold">Óleo de abóbora</span>
+        <p class="text-xs text-gray-700">ID: 23432252</p>
+      </div>
+      <span class="font-bold  text-green-600">R$25.99</span>
+    </div>
+    <button class="hover:bg-green-700 text-gray-50 bg-green-500 py-2 rounded-md cursor-pointer">Adicionar ao carrinho</button>
+  </div>
+</div>
+
+<div class="w-60 h-80 bg-gray-50 p-3 flex flex-col gap-1 rounded-2xl">
+
+  
+  <div class="h-48">
+    <img src="./carIcon.png" alt="" />
+  </div>
+
+
+
+  <div class="flex flex-col gap-4">
+    <div class="flex flex-row justify-between">
+      <div class="flex flex-col">
+        <span class="text-xl font-bold">Óleo de abóbora</span>
+        <p class="text-xs text-gray-700">ID: 23432252</p>
+      </div>
+      <span class="font-bold  text-green-600">R$25.99</span>
+    </div>
+    <button class="hover:bg-green-700 text-gray-50 bg-green-500 py-2 rounded-md cursor-pointer">Adicionar ao carrinho</button>
+  </div>
+</div>
+
+<div class="w-60 h-80 bg-gray-50 p-3 flex flex-col gap-1 rounded-2xl">
+
+  
+  <div class="h-48">
+    <img src="./carIcon.png" alt="" />
+  </div>
+
+
+
+  <div class="flex flex-col gap-4">
+    <div class="flex flex-row justify-between">
+      <div class="flex flex-col">
+        <span class="text-xl font-bold">Óleo de abóbora</span>
+        <p class="text-xs text-gray-700">ID: 23432252</p>
+      </div>
+      <span class="font-bold  text-green-600">R$25.99</span>
+    </div>
+    <button class="hover:bg-green-700 text-gray-50 bg-green-500 py-2 rounded-md cursor-pointer">Adicionar ao carrinho</button>
+  </div>
 
   
 </div>
--- janete-front/src/routes/AppRoutes.jsx
+++ janete-front/src/routes/AppRoutes.jsx
@@ -8,6 +8,7 @@ import Cadastro2 from "../pages/CadrastroP";
 import Dashboard from "../pages/DashboardPage";
 import Login from "../pages/LoginPage";
 import Checkout from "../pages/CheckoutPage";
+import Produtos from "../pages/ProdutosPage"
 
 export default function AppRoutes() {
   return (
@@ -16,6 +17,7 @@ export default function AppRoutes() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} /> 
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/produtos" element={<Produtos />} />
           <Route path="/checkout" element={<Checkout />} />
         </Route>
         
